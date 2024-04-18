3c2c70d5b890f05dd88b8f6be154d873360f2f29
Julio4647 <104937990+Julio4647@users.noreply.github.com>
2024-04-18 17:48:58 -0600

    first_Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="home">
+      <h1>Login con Auth</h1>
+      <div v-if="!$auth.loading">
+        <button v-if="$auth.isAuthenticated" @click="redireccionar">Perfil</button>
+        <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+        <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      </div>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "home",
+  
+    methods: {
+      login() {
+        this.$auth.loginWithRedirect();
+      },
+      logout() {
+        this.$auth.logout({
+          logoutParams: {
+            returnTo: window.location.origin,
+          },
+        });
+      },
+      redireccionar() {
+      this.$router.push('/profile');
+    }
+    },
+  };
+  </script>
